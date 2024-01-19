85af9dc3d29c64fd7ef22a62ac0fe6aaa90daeac
Michael Davenport <mdaven19@byu.edu>
2024-01-18 19:23:16 -0700

    add more tests

--- server/src/test/java/services/FailServiceTest.java
+++ server/src/test/java/services/FailServiceTest.java
@@ -3,6 +3,12 @@ package services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
 public class FailServiceTest {
     @Test
     public void meh1() {
@@ -42,5 +48,40 @@ public class FailServiceTest {
     @Test
     public void meh8() {
         Assertions.assertThrows(Exception.class, ()->{});
+    }
+
+    @Test
+    public void meh9() {
+        Assertions.fail();
+    }
+
+    @Test
+    public void meh10() {
+        Assertions.assertTimeout(Duration.ZERO, () -> Thread.sleep(1));
+    }
+
+    @Test
+    public void meh11() {
+        Assertions.assertSame("hi", "hello");
+    }
+
+    @Test
+    public void meh12() {
+        Assertions.assertNotSame("hi", "hi");
+    }
+
+    @Test
+    public void meh13() {
+        Assertions.assertIterableEquals(List.of(1), List.of(2));
+    }
+
+    @Test
+    public void meh14() {
+        Assertions.assertArrayEquals(new int[]{7}, new int[]{7, 9});
+    }
+
+    @Test
+    public void meh15() {
+        Assertions.assertInstanceOf(Integer.class, "hi");
     }
 }
