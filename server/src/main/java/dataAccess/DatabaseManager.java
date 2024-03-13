726605c632f4d2f2cbb1ce4f125c06f4a344426d
Michael Davenport <52420978+19mdavenport@users.noreply.github.com>
2024-03-12 19:19:21 -0600

    Update DatabaseManager.java

--- server/src/main/java/dataAccess/DatabaseManager.java
+++ server/src/main/java/dataAccess/DatabaseManager.java
@@ -6,7 +6,7 @@ import java.sql.*;
 import java.util.Properties;
 
 public class DatabaseManager {
-    private static final String databaseName;
+    private static String databaseName;
     private static final String user;
     private static final String password;
     private static final String connectionUrl;
