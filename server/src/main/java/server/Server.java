8459c6dd413562bfc897b7bf23d58026548935ca
Michael Davenport <52420978+19mdavenport@users.noreply.github.com>
2024-03-20 18:56:26 -0600

    Update Server.java

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -14,6 +14,7 @@ import websocket.WebSocketHandler;
 import java.net.HttpURLConnection;
 
 public class Server {
+    LOLOLOLOLOLOLOLOLOLOLOLOLOL
 
     public static void main(String[] args) {
         new Server().run(8080);
