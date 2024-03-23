42738382ed2b1bfca594e80bc549728433d365fb
Michael Davenport <mdaven19@byu.edu>
2024-03-22 20:45:25 -0600

    lol

--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -1,22 +1,17 @@
 package clientTests;
 
-import chess.ChessGame;
-import model.*;
-import org.junit.jupiter.api.*;
-import server.Server;
-import web.ResponseException;
-import web.ServerFacade;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
 
-import java.util.Random;
-import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 
 public class ServerFacadeTests {
-    int lol = 0;
+    int lol = 1;
     @TestFactory
     public Stream<DynamicTest> lol() {
-        return Stream.generate(() -> DynamicTest.dynamicTest("lol" + lol++, () -> Assertions.assertTrue(true))).limit(100);
+        return Stream.generate(() -> DynamicTest.dynamicTest("lol" + lol, () -> lol++)).limit(100);
     }
 
 }
